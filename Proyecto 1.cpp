172819e7781d13c42e7fa481c4f2b41c5a466651
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2023-08-10 12:36:20 -0600

    Update Proyecto 1.cpp

--- Proyecto 1.cpp
+++ Proyecto 1.cpp
@@ -6,24 +6,25 @@ int main (int argc,char **argv[])
 {
 	setlocale(LC_ALL,"");\Para colocar caracteres especiales
 	
-	printf("Primera GeneraciÛn (1940-1956)\n");
-	printf("CaracterÌsticas: \n");
-	printf("Grandes, costosas, programaciÛn a nivel de lenguaje de m·quina y cableado manual para la configuraciÛn. \n");
+	printf("Primera Generaci√≥n (1940-1956)\n");
+	printf("Caracter√≠sticas: \n");
+	printf("Grandes, costosas, programaci√≥n a nivel de lenguaje de m√°quina y cableado manual para la configuraci√≥n. \n");
 	
-	printf("Segunda GeneraciÛn (1956-1963)\n");
-	printf("CaracterÌsticas: \n");
-	printf("Se introdujo el lenguaje ensamblador y el sistema operativo, m·s pequeÒas y generaban menos calor.\n");
+	printf("Segunda Generaci√≥n (1956-1963)\n");
+	printf("Caracter√≠sticas: \n");
+	printf("Se introdujo el lenguaje ensamblador y el sistema operativo, m√°s peque√±as y generaban menos calor.\n");
 	
-	printf("Tercera GeneraciÛn (1964-1971)\n");
-	printf("CaracterÌsticas: \n");
-	printf("Mayor capacidad de procesamiento y menor tamaÒo, se introdujeron los sistemas operativos m·s avanzados, la multiprogramaciÛn.  \n");
+	printf("Tercera Generaci√≥n (1964-1971)\n");
+	printf("Caracter√≠sticas: \n");
+	printf("Mayor capacidad de procesamiento y menor tama√±o, se introdujeron los sistemas operativos m√°s avanzados, la multiprogramaci√≥n.  \n");
 	
-	printf("Cuarta GeneraciÛn (1971-1980)\n");
-	printf("CaracterÌsticas: \n");
-	printf("Uso generalizado de microprocesadores y chips de memoria, se popularizÛ la inform·tica personal y se expandiÛ el uso de lenguajes de programaciÛn de alto nivel.\n");
+	printf("Cuarta Generaci√≥n (1971-1980)\n");
+	printf("Caracter√≠sticas: \n");
+	printf("Uso generalizado de microprocesadores y chips de memoria, se populariz√≥ la inform√°tica personal y se expandi√≥ el uso de lenguajes de programaci√≥n de alto nivel.\n");
 	
-	printf("Quinta GeneraciÛn (desde 1980 en adelante)\n");
-	printf("CaracterÌsticas: \n");
-	printf("Avances en inteligencia artificial (IA) y procesamiento paralelo, uso de supercomputadoras para c·lculos cientÌficos y aplicaciones complejas, evoluciÛn de las interfaces de usuario y sistemas operativos.");
+	printf("Quinta Generaci√≥n (desde 1980 en adelante)\n");
+	printf("Caracter√≠sticas: \n");
+	printf("Avances en inteligencia artificial (IA) y procesamiento paralelo, uso de supercomputadoras para c√°lculos cient√≠ficos y aplicaciones complejas, evoluci√≥n de las interfaces de usuario y sistemas operativos.");
+	
 	return 0;
 }
