46a63096cb42358ad7b08e2db03876cf4f9eb0cb
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2023-08-15 11:47:53 -0600

    Add files via upload

--- Proyecto 1 - Generaciones de computadoras.cpp
+++ Proyecto 1 - Generaciones de computadoras.cpp
@@ -7,18 +7,18 @@
 
 int main (int argc,char **argv[])
 {
-	setlocale(LC_ALL,"spanish");//Para colocar caracteres especiales
+	setlocale(LC_ALL,"");//Para colocar caracteres especiales
 	/*system("cls"); //Para colocar color de letras (error con la otra forma)*/
 	
-	int clave;//Variable para ingresar despuÃ©s y seleccionar nÃºmero
+	int clave;//Variable para ingresar después y seleccionar número
  
 
- 	printf("\vÂ¡HOLA!\v");
+ 	printf("\v¡HOLA!\v");
  	printf("\nAntes de comenzar vamos a cambiar el color de la consola.\n");
- 	printf("\vEscriba el nÃºmero que desee de acuerdo a la tabla.\n\v");
+ 	printf("\vEscriba el número que deseé de acuerdo a la tabla.\n\v");
  
- 	printf("| %-10s | %-7s |\n", "Color", "NÃºmero");//Encabezados de la tabla
-	printf("|------------|---------|\n");//LÃ­nea intermedia
+ 	printf("| %-10s | %-7s |\n", "Color", "Número");//Encabezados de la tabla
+	printf("|------------|---------|\n");//Línea intermedia
 
     printf("| %-10s | %-7d |\n", "Verde", 1); //Filas de la tabla
     printf("| %-10s | %-7d |\n", "Azul", 2);
@@ -27,12 +27,12 @@ int main (int argc,char **argv[])
     printf("| %-10s | %-7d |\n", "Gris", 5);
     printf("| %-10s | %-7d |\n", "Blanco", 6);
     printf("| %-10s | %-7s |\n", "Negro","-");
- printf("Introduzca su nÃºmero por favor =):");
- scanf("%d",&clave);//Solicitar nÃºmero
+ printf("Introduzca su número por favor =):");
+ scanf("%d",&clave);//Solicitar número
  
-switch (clave){ /*Instrucciones dependiendo el nÃºmero elegido
+switch (clave){ /*Instrucciones dependiendo el número elegido
 
-!!!! 1er nÃºmero es para el fondo y 2do para el texto*/
+!!!! 1er número es para el fondo y 2do para el texto*/
 
 	case 1:
 	system("color 27");//Verde
@@ -63,31 +63,31 @@ switch (clave){ /*Instrucciones dependiendo el nÃºmero elegido
 	//system("color 1f"); Color de letras
 	
 	printf("\v--------------------------------------------------------------------------------------------\v\n");
-	printf("Primera GeneraciÃ³n (1940-1956)\n");
-	printf("CaracterÃ­sticas:\n");
-	printf("1.- Grandes y con procesadores lentos.\n2.- Costosas de producir.\n3.- ProgramaciÃ³n a nivel de lenguaje de mÃ¡quina y cableado manual para la configuraciÃ³n.\n");
+	printf("Primera Generación (1940-1956)\n");
+	printf("Características:\n");
+	printf("1.- Grandes y con procesadores lentos.\n2.- Costosas de producir.\n3.- Programación a nivel de lenguaje de máquina y cableado manual para la configuración.\n");
 	printf("\v--------------------------------------------------------------------------------------------\v\n");
 	
-	printf("Segunda GeneraciÃ³n (1956-1963)\n");
-	printf("CaracterÃ­sticas:\n");
-	printf("1.- Se introdujo el lenguaje ensamblador y el sistema operativo.\n2.- Eran mÃ¡s pequeÃ±as.\n3.- Generaban menos calor.\n");
+	printf("Segunda Generación (1956-1963)\n");
+	printf("Características:\n");
+	printf("1.- Se introdujo el lenguaje ensamblador y el sistema operativo.\n2.- Eran más pequeñas.\n3.- Generaban menos calor.\n");
 	printf("\v--------------------------------------------------------------------------------------------\v\n");
 	
-	printf("Tercera GeneraciÃ³n (1964-1971)\n");
-	printf("CaracterÃ­sticas:\n");
-	printf("1.- Mayor capacidad de procesamiento y menor tamaÃ±o\n2.- Se introdujeron los sistemas operativos mÃ¡s avanzados.\n3.- Surge la multiprogramaciÃ³n.\n");
+	printf("Tercera Generación (1964-1971)\n");
+	printf("Características:\n");
+	printf("1.- Mayor capacidad de procesamiento y menor tamaño\n2.- Se introdujeron los sistemas operativos más avanzados.\n3.- Surge la multiprogramación.\n");
 	printf("\v--------------------------------------------------------------------------------------------\v\n");
 		
-	printf("Cuarta GeneraciÃ³n (1971-1980)\n");
-	printf("CaracterÃ­sticas:\n");
-	printf("1.- Uso generalizado de microprocesadores y chips de memoria.\n2.- Se popularizÃ³ la informÃ¡tica personal.\n3.- Se expandiÃ³ el uso de lenguajes de programaciÃ³n de alto nivel.\n");
+	printf("Cuarta Generación (1971-1980)\n");
+	printf("Características:\n");
+	printf("1.- Uso generalizado de microprocesadores y chips de memoria.\n2.- Se popularizó la informática personal.\n3.- Se expandió el uso de lenguajes de programación de alto nivel.\n");
 	printf("\v--------------------------------------------------------------------------------------------\v\n");
 		
 	/* printf("\033[0;31m"); (Color rojo con 31)*/		
-	printf("Quinta GeneraciÃ³n (desde 1980 en adelante)\n");
+	printf("Quinta Generación (desde 1980 en adelante)\n");
 	/* printf("\033[0;0m"); (Color normal con 30)*/	
-	printf("CaracterÃ­sticas:\n");
-	printf("1.- Avances en inteligencia artificial (IA) y procesamiento paralelo.\n2.- Uso de supercomputadoras para cÃ¡lculos cientÃ­ficos y aplicaciones complejas.\n3.- EvoluciÃ³n de las interfaces de usuario y sistemas operativos.\n");
+	printf("Características:\n");
+	printf("1.- Avances en inteligencia artificial (IA) y procesamiento paralelo.\n2.- Uso de supercomputadoras para cálculos científicos y aplicaciones complejas.\n3.- Evolución de las interfaces de usuario y sistemas operativos.\n");
 	printf("\v--------------------------------------------------------------------------------------------\v\n");
 	return 0;
 }
