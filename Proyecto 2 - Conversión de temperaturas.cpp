7dac46f41121e1ec17cf2a5f74fef155d600730e
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2023-08-17 12:31:53 -0600

    Add files via upload

---
+++ Proyecto 2 - Conversión de temperaturas.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define FAHRENHEIT 9/5
+
+int main (){
+	
+	float temperatura;
+	printf ("¡HOLA!");
+	printf("Si desea ingresar grados Celsius a Fahrenheit presione 1\n");
+	printf("Si desea ingresar grados Celsius a Fahrenheit presione 0");
+	printf("Introduzca su número por favor:");
+	scanf("%f",&temperatura);
+	
+	return 0;
+	
+}
