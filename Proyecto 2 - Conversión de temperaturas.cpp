e2dfa8c71beace86ead81724d5d248100cfa9aa3
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2023-08-22 11:51:51 -0600

    Add files via upload

--- Proyecto 2 - Conversión de temperaturas.cpp
+++ Proyecto 2 - Conversión de temperaturas.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>//Idiomas
 #include <locale.h>//Idiomas principalmente
-#include <windows.h>//Colores y funciÃ³n Sleep
+#include <windows.h>//Colores y función Sleep
 
 int main ()
 {
@@ -12,23 +12,23 @@ int main ()
 	
 
 	/*Variables*/
-	int decision;//Variable declarada para elegir Â°C O Â°F (0 o 1)
+	int decision;//Variable declarada para elegir °C O °F (0 o 1)
 	float temperatura;//Variable declarada para guardar temp y mostrarla al final
 	float temperaturaFinal;//Variable declarada para guardar las operaciones
-	char gradosC[]="Â°C";
-	char gradosF[]="Â°F";
+	char gradosC[]="°C";
+	char gradosF[]="°F";
  
  
  	/*Letras aparecen poco a poco*/
-	char titulo[]={"\v\t\t\t\tÂ°F <---> Â°C\a\v"};//Variable de texto. \a = sonido
+	char titulo[]={"\v\t\t\t\t°F <---> °C\a\v"};//Variable de texto. \a = sonido
 	int i=0;//Declarar variable entera
 	for(i=0;i<18;i++){//Bucle que comienza en 0 e incrementa su valor siempre que sea menor que 1
 		printf("%c ",titulo[i]);
-		Sleep(100);//Pausa la ejecuciÃ³n 100 milisegundos
+		Sleep(100);//Pausa la ejecución 100 milisegundos
 	}
 
 
-	/*Indicar quÃ© opciones hay (ingresar 0 o 1)*/
+	/*Indicar qué opciones hay (ingresar 0 o 1)*/
 	printf("\v\tSi desea convertir grados Celsius a Fahrenheit presione 0\n");//%4s mover a la derecha
 	printf("\tSi desea convertir grados Fahrenheit a Celsius presione 1\v\v");
 	
@@ -37,8 +37,8 @@ int main ()
 	int l;//Declarar variable entera
     for (l = 0; l < 3; l++) {//Se ejecuta 3 veces
         
-		printf("\rIntroduzca 0 Ã³ 1 por favor:");//\r mueve el cursor al principio de la lÃ­nea (borra lo anterior)
-        fflush(stdout);//EnvÃ­a texto anterior a la consola
+		printf("\rIntroduzca 0 ó 1 por favor:");//\r mueve el cursor al principio de la línea (borra lo anterior)
+        fflush(stdout);//Envía texto anterior a la consola
         Sleep(800); // Tiempo en pantalla (1000 milisegundos=1 segundo)
         printf("\r                               ");
         fflush(stdout);
@@ -47,40 +47,40 @@ int main ()
 
 
 	/*Introducir 0 o 1*/
-	printf("\rIntroduzca 0 Ã³ 1 por favor:");
+	printf("\rIntroduzca 0 ó 1 por favor:");
 	scanf("%d",&decision);//Usuario debe introducir 1 o 2
 	
 	
-	/*Condiciones dependiendo del nÃºmero ingresado*/
-	if(decision==0){//CondiciÃ³n para 1
+	/*Condiciones dependiendo del número ingresado*/
+	if(decision==0){//Condición para 1
 	
 	printf("\v\vIngrese grados Celsius:");
-	scanf("%f",&temperatura);//Ingresar Â°C
+	scanf("%f",&temperatura);//Ingresar °C
 	
 	printf("\033[1;37m");//Bold blanco
 	printf("\033[46m");//Fondo Cyan
-	temperaturaFinal =(temperatura*9/5)+32;//Â°C-->Â°F
+	temperaturaFinal =(temperatura*9/5)+32;//°C-->°F
 	printf("\v%.2f %s equivalen a %.2f %s", temperatura, gradosC, temperaturaFinal, gradosF);//Mensaje final de equivalencia
 }
-	else if (decision==1){//CondiciÃ³n para 1
+	else if (decision==1){//Condición para 1
 
 	printf("\v\vIngrese grados Fahrenheit:");
-	scanf("%f",&temperatura);//Ingresar Â°F
+	scanf("%f",&temperatura);//Ingresar °F
 	
 	printf("\033[1;37m");//Bold blanco
 	printf("\033[46m");//Fondo Cyan
-	temperaturaFinal =(temperatura-32)*5/9;//Â°F-->Â°C
+	temperaturaFinal =(temperatura-32)*5/9;//°F-->°C
 	printf("\v%.2f %s equivalen a %.2f %s",temperatura, gradosF, temperaturaFinal, gradosC);//%.2f = 2 puntos decimales
 }
-	else{//CondiciÃ³n para todo lo demÃ¡s =! 0 Ã³ 1
+	else{//Condición para todo lo demás =! 0 ó 1
 	
 	printf("\033[1;37m");//Bold blanco
 	printf("\033[46m");//Fondo Cyan
-	printf("\v\vÂ¡Error!\nDebe introducir 0 Ã³ 1.");
+	printf("\v\v¡Error!\nDebe introducir 0 ó 1.");
 	}
 	
-	//https://gist.github.com/vratiu/9780109 = link colores 
-	//https://gist.github.com/abritinthebay/d80eb99b2726c83feb0d97eab95206c4 = link cÃ³digos de escape
+	// https://gist.github.com/vratiu/9780109 = link colores
+	// https://gist.github.com/abritinthebay/d80eb99b2726c83feb0d97eab95206c4 = link códigos de escape
 	
 	return 0;
 }
